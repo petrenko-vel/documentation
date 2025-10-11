e7db1dcb7e78f9c1f9ba67154011a3453ec2313e
Vaaleria Yatsukk <proslera@yandex.ru>
2025-10-11 22:48:22 +0300

    Очистка ветки main

--- src/modules/PromoClose.js
+++
@@ -1,6 +0,0 @@
-// const promo = document.querySelector('[data-js-promo]');
-// const close = document.querySelector('[data-js-close]');
-
-// close.addEventListener("click", () => {
-//     promo.style.display = "none";
-// });
