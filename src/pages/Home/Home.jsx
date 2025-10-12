8c63a54b28889b8ac2e90cf399d5ab2f7e0cfa52
Valeria Yatsuk <proslera@yandex.ru>
2025-10-12 13:21:26 +0300

    ADD: разработка hero на главной странице

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom'; 
+import logo from '@/assets/image/logo.svg'
 import './Home.scss'
+import Logo from '@/components/Logo';
 
 export default function Home() {
     return (
-        <div className='container'>
-          <h1>Главная страница</h1>
-          <p>Контент работает!</p>
-        </div>
+        <>
+        <section className="offer">
+          <div className="offer__inner container">
+            <div className="offer__logo-inner">
+              <Logo />
+            </div>
+            <h1 className="offer__title">Умная лаборатория начинается здесь</h1>
+            <p className="offer__decriprtion">
+              Пусть Signal-master возьмёт на себя рутину — от поиска документации до управления
+            </p>
+           <div className="offer__links">
+             <Link to={"/docs"} className='offer__link'>Начать</Link>
+              <Link to={"/telegram"} className='offer__link'>Присоединиться к комьюнити</Link>
+           </div>
+          </div>
+        </section>
+        </>
     )
 }
